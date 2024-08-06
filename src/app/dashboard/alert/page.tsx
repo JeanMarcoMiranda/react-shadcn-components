a15ce2625fdf7b76054de5052bf5335208660ce9
JeanMarcoMiranda <elecord2014@gmail.com>
2024-08-06 14:00:24 -0500

    feat: alert seccion added

--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,8 +1,30 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { RocketIcon } from "@radix-ui/react-icons";
 
 export default function Page() {
   return (
-    <div>
-      <h1>Alert Page</h1>
+    <div className="grid gap-3">
+      <Alert>
+        <RocketIcon className="h-4 w-4 m-auto" />
+        <AlertTitle>Alert default variant</AlertTitle>
+        <AlertDescription>
+          This is the default variant of the alert
+        </AlertDescription>
+      </Alert>
+      <Alert variant="destructive">
+        <RocketIcon className="h-4 w-4" />
+        <AlertTitle>Alert destructive variant</AlertTitle>
+        <AlertDescription>
+          This is the destructive variant of the alert
+        </AlertDescription>
+      </Alert>
+      <Alert variant="success">
+        <RocketIcon className="h-4 w-4" />
+        <AlertTitle>Alert success variante</AlertTitle>
+        <AlertDescription>
+          This is the success variant of the alert
+        </AlertDescription>
+      </Alert>
     </div>
   );
 }
